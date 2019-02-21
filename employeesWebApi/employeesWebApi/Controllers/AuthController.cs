03f66fda1ddf75aad97ac4863590893c98a8a529
laigOSU <35331156+laigOSU@users.noreply.github.com>
2019-02-21 14:56:16 -0800

    Add files via upload

---
+++ employeesWebApi/employeesWebApi/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace employeesWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]   //So to access: api/auth/token to access this endpoint of our api
+        public ActionResult GetToken()
+        {
+
+            // 1. Need security key, which we use to sign the token, s.t. we can validate it later
+            // Just a string. Can save it anywhere in DB, config file, or envt. 
+            // Let's put in envt s.t. we won't accidentally commit it to code version server, and it 
+            // will always be secured. But later, put this key somewhere safe.
+            string securityKey = "this_is_the_very_long_security_key_for_token_validation_keep_this_a_secret_somehow";
+
+            // 2. Need symmetric security key
+            // we pass our securityKey to it.
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+
+            // 3. Need to create credentials for signing the token
+            // Pass our symmetricSecurityKey. Then choose algo for generating, sign, and validate the token 
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+
+            // 3a. Add claims (user type in JWT payload)
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Role, "Admin"));  // Don't forget to assign this claim to the token body, in #4.
+            claims.Add(new Claim(ClaimTypes.Role, "User"));
+            claims.Add(new Claim("Custom_Claim", "Custom_value"));
+
+
+            // 4. Need to create the token
+            var token = new JwtSecurityToken(
+                issuer: "theIssuer", //any string
+                audience: "readers",
+                expires: DateTime.Now.AddHours(1), // expires in 1 hr
+                signingCredentials: signingCredentials,
+                claims: claims
+                );
+
+            // 5. Return token
+            // We need the JWT SecurityTokenHandler to return, from this token, a string that can be used in request to our API
+            // The WriteToken will return string version of token, which we return to the client
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token)); 
+        }
+
+    }
+}
---
+++ employeesWebApi/employeesWebApi/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace employeesWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+
+        // Introduce JWT token authorization scheme into our application, under startup.cs under method configure services.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // From AuthController.cs
+            string securityKey = "this_is_the_very_long_security_key_for_token_validation_keep_this_a_secret_somehow";
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            // Add auto authentication scheme
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // What to validate
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+
+                        // Setup Validate Data
+                        ValidIssuer = "theIssuer",
+                        ValidAudience = "readers",
+                        IssuerSigningKey = symmetricSecurityKey
+                    };
+                });
+
+            // Set some options to be used to validate our token
+               services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
